2df3608846f5dc950c13ac4de54289c0ed70cdc0
Jesse Hayward <jessehayward@hookd.app>
2026-04-27 10:36:59 +1000

    External IP usage - for Hosting overseas

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -66,9 +66,11 @@ export async function POST(req: Request) {
         sendUpdate({ type: "info", progress: 1, message: "Fetching footage page..." });
 
         const headers = {
-          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-          "Accept-Language": "en-US,en;q=0.5"
+          "Accept-Language": "en-AU,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
+          "X-Forwarded-For": "1.128.0.1",
+          "X-Real-IP": "1.128.0.1"
         };
         const matchHtmlResponse = await fetch(url, { headers });
         if (!matchHtmlResponse.ok) {
